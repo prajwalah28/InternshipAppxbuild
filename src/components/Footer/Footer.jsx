3d2e7dd8bbf4bd160e94913ccf1b468274e94fb5
prajwaldeveloper <prajwalhelunde@gmail.com>
2024-02-14 21:29:37 +0530

    Add new code to the repository

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+import logo from '../../assets/images/logo.png'
+import {RiLinkedinFill} from 'react-icons/ri'
+import {AiFillYoutube,AiFillGithub,AiOutlineInstagram} from 'react-icons/ai';
 
+const socialLinks = [
+  {
+    path: "",
+    icon:<AiFillYoutube/>
+  }
+];
+const quickLinks =[
+  {
+    path:"/",
+    display:"Find a doctor",
+  }
+]
 export default function Footer() {
   return (
     <div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import "./Home.css";
 
-export default function Home() {
+import d1 from "../assets/images/doctor-img01.png";
+import d2 from "../assets/images/doctor-img02.png";
+import d3 from "../assets/images/doctor-img03.png";
+
+export default function Home(params) {
   return (
-    <div>
-      home
-    </div>
-  )
+    <>
+      <div className="main_class">
+        <div className="class1">
+          <div className="one">
+            <h1>We help patient live a healthy, longer life</h1>
+          </div>
+          <div className="two">
+            <div className="tone">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+                vero quas repellat quis veniam porro voluptatum, rem consequatur
+                quasi ipsam! vero quas repellat quis veniam porro voluptatum,
+                rem consequatur quasi ipsam!
+              </p>
+              <button>Request an Appointment</button>
+            </div>
+          </div>
+          <div className="three">
+            <h2 className="c1">30+</h2>
+            <h2 className="c2">15+</h2>
+            <h2 className="c3">100%</h2>
+          </div>
+
+          <div className="four">
+            <h5>Year Of Experience</h5>
+            <h5>Clinic location</h5>
+            <h5>Patient Satisfaction</h5>
+          </div>
+        </div>
+
+        <div className="class2">
+          <div className="img1">
+            <img src={d1} />
+          </div>
+          <div className="img2">
+            <img src={d2} />
+            <img src={d3} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
