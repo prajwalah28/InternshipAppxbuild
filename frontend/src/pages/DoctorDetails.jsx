0f26639162f3201db8ad26244a4fde844aa72dec
Sanketbankar <sanketsbankar33@gmail.com>
2024-02-15 17:17:13 +0530

    add

---
+++ frontend/src/pages/DoctorDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DoctorDetails(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default DoctorDetails;
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import {doctors} from "../assets/data/doctors.js";
 import DoctorCard from "../components/Doctor/DoctorCard.jsx";
+import './FindaDoctor.css'
 
 function Doctors() {
     return (
         <>
-        <section>
+        <section className="find">
             <div>
                 <h2>Find a Doctor</h2>
             </div>
             <div>
-                <input type="search"/>
+                <input type="search" placeholder="Search by doctor name or specification"/>
                 <button>Search</button>
             </div>
         </section>
             <section>
                 <div>
-                    <div>
+                    <div className="doctor-card">
                         {doctors.map((doctor) => (
                             <DoctorCard key={doctor.id} doctor={doctor}/>
                         ))}
