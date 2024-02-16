e24bfc0393b6d824e1faece412439805b71b0126
Sanketbankar <sanketsbankar33@gmail.com>
2024-02-16 18:37:27 +0530

    ghe bbu1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ export default function Home(params) {
       <div className="main_class">
         <div className="class1">
           <div className="one">
-              <h1>We help patient live a healthy, longer life</h1><p>baburao</p>
+              <h1>We help patient live a healthy, longer life</h1><p>baburao1ß</p>
           </div>
           <div className="two">
             <div className="tone">
